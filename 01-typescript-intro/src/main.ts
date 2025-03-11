4114ca7be50c5ee030f7795d49c7b3ffc0510d49
Carlos Calzadilla <maleras24@gmail.com>
2025-03-11 16:25:07 +0000

    First commit, Comienzo de curso

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,11 @@
+import './style.css';
+import './topics/02-object-interface';
+
+// import './topics/01-basic-types';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Hola Mundo';
+
+console.log('Hola mundo!!!');
+
